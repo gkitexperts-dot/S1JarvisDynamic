f9f5b635756ded2a807b894a6fc36d6e2e329868
gkitexperts-dot <gkitexperts@gmail.com>
2026-08-27 17:45:52 +0300

    Clean generated files and restore runtime source

---
+++ S1Jarvis.Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("S1Jarvis.Runtime")]
+[assembly: AssemblyDescription("Embedded Jarvis runtime and WebView2 UI")]
+[assembly: AssemblyCompany("S1Jarvis")]
+[assembly: AssemblyProduct("S1Jarvis")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7B1D0DC4-9E2C-4BB7-A869-5E58EC8B4D6D")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
